3d5f581307e29ee5d617fb9fb4a155cc60d5059d
dafnevk <d.vankuppevelt@esciencecenter.nl>
2018-02-22 10:51:39 +0100

    Added network statistics

--- nwtools/community.py
+++ nwtools/community.py
@@ -3,10 +3,12 @@ import collections
 
 def partition_statistics(partition, graph, weight=None):
     '''
-
+    Calculates statistics for each partition of a network.
+    Treats the network as undirected.
+    
     :param partition: dict {node: community}
-    :param graph:
-    :return:
+    :param graph: networkx graph
+    :return: statistics
     '''
 
     reverse_partition = collections.defaultdict(list)
@@ -17,5 +19,17 @@ def partition_statistics(partition, graph, weight=None):
     for c in reverse_partition:
         subgraph = graph.subgraph(reverse_partition[c])
         statistics[c]['size'] = nx.number_of_nodes(subgraph)
+        statistics[c]['total_degree'] = sum(graph.degree(reverse_partition[c]).values())
         statistics[c]['internal_degree'] = nx.number_of_edges(subgraph)
+        statistics[c]['average_internal_degree'] = statistics[c]['internal_degree'] / statistics[c]['size']
+        statistics[c]['external_degree'] = statistics[c]['total_degree'] - statistics[c]['internal_degree']
+        statistics[c]['average_external_degree'] = statistics[c]['external_degree'] / statistics[c]['size']
+        statistics[c]['conductance'] = statistics[c]['external_degree'] / statistics[c]['total_degree']
+        if weight is not None:
+            statistics[c]['total_strength'] = sum(graph.degree(reverse_partition[c], weight).values())
+            statistics[c]['internal_strength'] = sum(nx.get_edge_attributes(subgraph, weight).values())
+            statistics[c]['average_internal_strength'] = statistics[c]['internal_strength'] / statistics[c]['size']
+            statistics[c]['external_strength'] = statistics[c]['total_strength'] - statistics[c]['internal_strength']
+            statistics[c]['average_external_strength'] = statistics[c]['external_strength'] / statistics[c]['size']
+            statistics[c]['weighted_conductance'] = statistics[c]['external_strength'] / statistics[c]['total_strength']
     return statistics
