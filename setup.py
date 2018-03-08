e5ef956be4a93e9124cba9b7ca2962f4f18c79aa
dafnevk <d.vankuppevelt@esciencecenter.nl>
2018-03-08 17:04:51 +0100

    Renamed some stuff and added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+    
+setup(
+    name = "nwtools",
+    version = "0.1.0",
+    description = ("Analysis tools for networks"),
+    license = "Apache 2.0",
+    keywords = "Python",
+    url = "https://github.com/dafnevk/network-tools",
+    packages=['nwtools'],
+    install_requires=required,
+    long_description=read('README.md'),
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+    ],
+)
